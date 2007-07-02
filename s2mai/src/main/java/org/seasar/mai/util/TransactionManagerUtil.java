d649e9f8e494d851cc8cce92e7711c50869d005d
Satoshi Kimura <satoshi.kimura@gmail.com>
2007-07-02 13:52:53 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2mai/trunk@119 7daea1d6-8021-4d9a-90e6-59bcd598cd3a

--- s2mai/src/main/java/org/seasar/mai/util/TransactionManagerUtil.java
+++ s2mai/src/main/java/org/seasar/mai/util/TransactionManagerUtil.java
@@ -20,6 +20,8 @@ import javax.transaction.SystemException;
 import javax.transaction.Transaction;
 import javax.transaction.TransactionManager;
 
+import org.seasar.framework.exception.SystemRuntimeException;
+
 /**
  * @author Satsohi Kimura
  */
@@ -31,7 +33,7 @@ public class TransactionManagerUtil {
         try {
             return transactionManager.getStatus() != Status.STATUS_NO_TRANSACTION;
         } catch (SystemException e) {
-            throw new IllegalStateException(e);
+            throw new SystemRuntimeException(e);
         }
     }
 
