aa6be001ec0cd412f40399a0b2e41b329c82e41e
rokugensai <rokugensai@gmail.com>
2008-01-11 03:43:14 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/s2mai/tags/2008-01-11/0.9.6@197 7daea1d6-8021-4d9a-90e6-59bcd598cd3a

---
+++ s2mai/src/main/java/org/seasar/mai/property/mail/impl/MailPropertyWriterFactoryImpl.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.mai.property.mail.impl;
+
+import org.seasar.framework.container.ComponentNotFoundRuntimeException;
+import org.seasar.framework.container.S2Container;
+import org.seasar.mai.S2MaiConstants;
+import org.seasar.mai.property.mail.MailPropertyWriter;
+import org.seasar.mai.property.mail.MailPropertyWriterFactory;
+
+/**
+ * @author rokugen
+ */
+public class MailPropertyWriterFactoryImpl implements MailPropertyWriterFactory, S2MaiConstants {
+    private S2Container container;
+
+    public MailPropertyWriter getMailPropertyWriter(String propName) {
+        
+        if(isMailProperty(propName) == false){
+            return null;
+        }
+        
+        try {
+            return (MailPropertyWriter) container.getComponent(propName + "Writer");
+        } catch (ComponentNotFoundRuntimeException ignore) {
+        }
+        return null;
+    }
+    
+    protected boolean isMailProperty(String propName){        
+        for(int i =0; i < MAIL_PROPERTIES.length; i++){
+            if(MAIL_PROPERTIES[i].equals(propName)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param container
+     *            The container to set.
+     */
+    public final void setContainer(S2Container container) {
+        this.container = container;
+    }
+
+}
---
+++ s2mai/src/test/java/org/seasar/mai/interceptors/LineSeparatorTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2004-2007 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.mai.interceptors;
+
+import org.seasar.extension.unit.S2TestCase;
+import org.seasar.mai.unit.SendMailTestUtil;
+
+import com.ozacc.mail.Mail;
+
+/**
+ * @author Satsohi Kimura
+ */
+public class LineSeparatorTest extends S2TestCase {
+    private LineSeparatorTestMai mai;
+
+    protected void setUp() throws Exception {
+        include("test.dicon");
+        SendMailTestUtil.init();
+    }
+
+    public void testCrLf() {
+        mai.sendMailCrLf();
+
+        Mail mail = SendMailTestUtil.getActualMail(0);
+        assertEquals("Test", mail.getSubject());
+        assertEquals("MailBody\r\n", mail.getText());
+    }
+
+    public void testCr() {
+        mai.sendMailCr();
+
+        Mail mail = SendMailTestUtil.getActualMail(0);
+        assertEquals("Test", mail.getSubject());
+        assertEquals("MailBody\r", mail.getText());
+    }
+
+    public void testLf() {
+        mai.sendMailLf();
+
+        Mail mail = SendMailTestUtil.getActualMail(0);
+        assertEquals("Test", mail.getSubject());
+        assertEquals("MailBody\n", mail.getText());
+    }
+}
